149d540bc101ee243bc0c7f1182f0d60ab2e5edd
Tomas Larson <tomas.g.larson@gmail.com>
2017-01-03 11:41:42 -0600

    New version

--- src/Main.java
+++ src/Main.java
@@ -7,31 +7,32 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		//boy.txt and girl.txt default to current directory
+		String type = arge[0];
 		String path = "";
-
-		String namegender = "boy";
-		int length = 0;
-
-		while (!namegender.equals("stop")) {
-
-			Scanner in = new Scanner(System.in);
-			System.out.println("Boy or Girl? ");
-			namegender = in.nextLine();
-
-			System.out.println("How long should the name be? ");
-			length = in.nextInt();
+		int length = 5;
 
 
-			NameList namelist = new NameList(namegender, path);
-			HashMap<String, Integer> nameHM = namelist.getHM(namegender);
+
+		// while (!namegender.equals("stop")) {
+
+			// Scanner in = new Scanner(System.in);
+			// System.out.println("Boy or Girl? ");
+			// namegender = in.nextLine();
+
+			// System.out.println("How long should the name be? ");
+			// length = in.nextInt();
+
+
+
+			NameList namelist = new NameList(type, path);
+			HashMap<String, Integer> nameHM = namelist.getHM(type);
 
 			NameGenerator namegen = new NameGenerator();
 			namegen.setParameters(nameHM, length);
 
 			String name = namegen.generate();
-			System.out.println(name);
-			System.out.println("");
-		}
+			System.out.print(name);
+		// }
 
 	}
 
